21290ccd17339a3213010532fdd698f63bb29429
retsrif <retsrif@gmail.com>
2012-01-09 18:54:21 +0300

    partially completed join/leave network

--- src/main/java/org/mcexchange/RegisteredPackets.java
+++ src/main/java/org/mcexchange/RegisteredPackets.java
@@ -7,12 +7,15 @@ package org.mcexchange;
 public class RegisteredPackets {
 	private DisconnectPacket disconnect;
 	private MessagePacket message;
+	private JoinLeavePacket joinleave;
 	
 	public RegisteredPackets(ClientConnection sc) {
 		disconnect = new DisconnectPacket(sc);
 		Packet.assignId((byte)-127, disconnect);
 		message = new MessagePacket(sc);
 		Packet.assignId((byte)-126, message);
+		joinleave = new JoinLeavePacket(sc);
+		Packet.assignId((byte)-125, joinleave);
 	}
 	
 	public Packet getDisconnect() {
@@ -21,5 +24,9 @@ public class RegisteredPackets {
 	
 	public MessagePacket getMessage() {
 		return message;
+	}
+	
+	public JoinLeavePacket getJoinLeave() {
+		return joinleave;
 	}
 }
